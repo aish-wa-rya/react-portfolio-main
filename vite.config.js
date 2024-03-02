800db19b561a909b45341db2228eabd023391411
Aishwarya Shrestha <aaishwaryashresthaa@gmail.com>
2024-03-02 10:33:56 +0545

    add: workflor

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  base: "/react-portfolio-main/",
   plugins: [react()],
   css: {
     modules: {
       localsConvention: "camelCase",
     },
-  },
+  }
 });
